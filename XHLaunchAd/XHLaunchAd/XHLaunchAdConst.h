54d0d622aa6d3b21e4bf16e4d8475c7fdec60f6f
Miles <liyida188@163.com>
2023-08-08 09:43:40 +0800

    X fix

--- XHLaunchAd/XHLaunchAd/XHLaunchAdConst.h
+++ XHLaunchAd/XHLaunchAd/XHLaunchAdConst.h
@@ -15,11 +15,11 @@
 #define XH_ScreenW    [UIScreen mainScreen].bounds.size.width
 #define XH_ScreenH    [UIScreen mainScreen].bounds.size.height
 
-#define XH_IPHONEX  ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1125, 2436), [[UIScreen mainScreen] currentMode].size) : NO)
-#define XH_IPHONEXR    ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(828, 1792), [[UIScreen mainScreen] currentMode].size) : NO)
-#define XH_IPHONEXSMAX    ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1242, 2688), [[UIScreen mainScreen] currentMode].size) : NO)
-#define XH_FULLSCREEN ((XH_IPHONEX || XH_IPHONEXR || XH_IPHONEXSMAX) ? YES : NO)
+#define XH_FULLSCREEN [XHLaunchAdConst iPhoneX]
 
+#define XH_KEYWINDOW [XHLaunchAdConst getKeyWindow]
+
+#define XH_SAFEAREAINSETS XH_KEYWINDOW.safeAreaInsets
 
 #define XHISURLString(string)  ([string hasPrefix:@"https://"] || [string hasPrefix:@"http://"]) ? YES:NO
 #define XHStringContainsSubString(string,subString)  ([string rangeOfString:subString].location == NSNotFound) ? NO:YES
@@ -84,4 +84,10 @@ UIKIT_EXTERN NSString *const XHLaunchAdVideoPlayFailedNotification;
 UIKIT_EXTERN BOOL XHLaunchAdPrefersHomeIndicatorAutoHidden;
 
 
+@interface XHLaunchAdConst : NSObject
 
+
++(UIWindow *)getKeyWindow;
++(BOOL)iPhoneX;
+
+@end
--- XHLaunchAd/XHLaunchAd/XHLaunchAdConst.m
+++ XHLaunchAd/XHLaunchAd/XHLaunchAdConst.m
@@ -20,3 +20,57 @@ NSString *const XHLaunchAdVideoPlayFailedNotification = @"XHLaunchAdVideoPlayFai
 
 BOOL XHLaunchAdPrefersHomeIndicatorAutoHidden = NO;
 
+
+
+@implementation XHLaunchAdConst
+
++(UIWindow *)getKeyWindow{
+    
+    if (@available(iOS 13.0 , *)){
+        
+        NSSet * scenes = UIApplication.sharedApplication.connectedScenes;
+        
+        for (UIWindowScene *sen in scenes) {
+            if ([sen isKindOfClass:[UIWindowScene class]] && sen.activationState == UISceneActivationStateForegroundActive){
+                UIWindow *window = sen.windows.firstObject;
+                if([window isKindOfClass:[UIWindow class]]){
+                    return window;
+                }
+            }
+        }
+    }
+    
+    if (@available(iOS 13.0 , *)){
+        
+        UIWindowScene *windowScene  = (UIWindowScene*)UIApplication.sharedApplication.connectedScenes.allObjects.firstObject;
+        if([windowScene isKindOfClass:[UIWindowScene class]] && [windowScene.windows.firstObject isKindOfClass:[UIWindow class]]){
+            return  windowScene.windows.firstObject;
+        }
+    }
+    
+    if (@available(iOS 16.0 , *)){
+        if ([UIApplication.sharedApplication.windows.firstObject isKindOfClass:[UIWindow class]]){
+            return  UIApplication.sharedApplication.windows.firstObject;
+        }
+    }
+    
+    UIWindow *window = UIApplication.sharedApplication.delegate.window;
+    if (window && [window isKindOfClass:[UIWindow class ]]){
+        return  window;
+    }
+    
+    return  UIApplication.sharedApplication.keyWindow;
+}
+
+
++(BOOL)iPhoneX{
+    
+    if (@available(iOS 11.0 , *)){
+        return  XH_SAFEAREAINSETS.bottom > 0 ? YES : NO;
+    }
+    return NO;
+}
+
+
+
+@end
